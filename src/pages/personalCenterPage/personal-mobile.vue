2964c6715823d475a80cbb4e0a120da51c14e0fd
tiamoyajie <years8023@gmail.com>
2017-04-20 17:32:26 +0800

    test commit

---
+++ src/pages/personalCenterPage/personal-mobile.vue
@@ -0,0 +1,130 @@
+<template>
+	<div class="personal-mobile">
+		<div class="personal-mobile-info">
+			<p>1、更换手机号码需要先做登录密码校验，请输入当前帐号登录密码；</p>
+			<p>2、更换号码后，下次登录使用新号码登录，当前手机号 <span>{{ mobile }}</span>。</p>
+		</div>
+		<div class="personal-mobile-wrap" v-if="isShow">
+			<label for="password">登录密码</label>
+			<input type="password" name="password" id="password" placeholder="请输入登录密码" />
+			<button>校验</button>
+		</div>
+		<div class="personal-mobile-wrap" v-if="!isShow">
+			<div class="personal-mobile-item">
+				<label for="newMobile">新手机号</label>
+				<input type="text" name="newMobile" id="newMobile" placeholder="请输入您的新手机号" />
+				<button>获取验证码</button>
+			</div>
+			<div class="personal-mobile-item">
+				<label for="sbCode">验证码</label>
+				<input type="text" name="sbCode" id="sbCode" placeholder="请输入4位验证码" />
+			</div>
+			<button class="btnSubmit">保存</button>
+		</div>
+		<div class="personal-mobile-alert" v-if="alertShow">
+			<i>—</i>登录密码检验失败
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				mobile: 18333604006,
+				alertShow: false,
+				isShow: false
+			};
+		}
+	};
+</script>
+
+<style lang="scss" scoped="scoped">
+	.personal-mobile-info {
+		margin: 0 auto; 
+		padding-top: 20px;
+		width: 884px;
+		background: #FFF7E3;
+		border: 1px solid #d1d1d1;
+		border-top: 0;
+		p {
+			padding-left: 120px;
+			font-size: 14px;
+			color: #666;
+			line-height: 28px;
+			span {
+				color: #ff8400;
+			}
+			&:last-of-type {
+				padding-bottom: 20px;
+			}
+		}
+	}
+	
+	.personal-mobile-wrap {
+		margin-top: 35px;
+		label {
+			display: inline-block;
+			padding-left: 120px;
+			width: 106px;
+			font-size: 16px;
+			line-height: 19px;
+			color: #333;
+		}
+		input {
+			padding: 4px 10px 0;
+			width: 320px;
+			height: 30px;
+			border: 0;
+			border: 1px solid #d1d1d1;
+			border-radius: 3px;
+		}
+		button {
+			margin-left: 25px;
+			width: 88px;
+			height: 32px;
+			background: #4C93FD;
+			color: #fff;
+			border: 0;
+			border: 1px solid #4C93FD;
+			border-radius: 3px;
+			cursor: pointer;
+		}
+		.personal-mobile-item {
+			margin-bottom: 20px;
+			button {
+				width: 120px;
+				height: 32px;
+				color: #4C93FD;
+				font-size: 16px;
+				line-height: 32px;
+				background: #fff;
+				border: 0;
+				border: 1px solid #4C93FD;
+			}
+		}
+		.btnSubmit {
+			margin-left: 230px;
+		}
+	}
+	
+	.personal-mobile-alert {
+		margin-left: 230px;
+		margin-top: 15px;
+		color: #ff8400;
+		font-size: 14px;
+		line-height: 16px;
+		i {
+			display: inline-block;
+			margin-right: 10px;
+			width: 14px;
+			height: 14px;
+			background: #FF8400;
+			border-radius: 50%;
+			color: #FFF;
+			font-size: 12px;
+			text-align: center;
+			line-height: 14px;
+		}
+	}
+</style>
---
+++ src/pages/personalCenterPage/personal-password.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="personal-password">
+		<div class="personal-password-wrap">
+			<label for="password">原密码</label>
+			<input type="password" name="password" id="password" placeholder="请输入账户原密码" />
+		</div>
+		<div class="personal-password-wrap">
+			<label for="passwordNew">新密码</label>
+			<input type="password" name="passwordNew" id="passwordNew" placeholder="请输入新密码" />
+		</div>
+		<div class="personal-password-wrap">
+			<label for="passwordNew1">确认密码</label>
+			<input type="password" name="passwordNew1" id="passwordNew1" placeholder="请确认新密码" />
+		</div>
+		<button>保存</button>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style lang="scss" scoped="scoped">
+	.personal-password {
+		margin-top: 50px;
+		.personal-password-wrap {
+			margin-top: 20px;
+			label {
+				display: inline-block;
+				padding-left: 120px;
+				width: 106px;
+				font-size: 16px;
+				line-height: 19px;
+				color: #333;
+			}
+			input {
+				padding: 0 10px;
+				width: 320px;
+				height: 30px;
+				border: 0;
+				border: 1px solid #d1d1d1;
+				border-radius: 3px;
+			}
+		}
+		button {
+			margin-top: 20px;
+			margin-left: 230px;
+			width: 88px;
+			height: 32px;
+			background: #4C93FD;
+			color: #fff;
+			border: 0;
+			border: 1px solid #4C93FD;
+			border-radius: 3px;
+			cursor: pointer;
+		}
+	}
+</style>
