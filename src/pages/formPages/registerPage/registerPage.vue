13e4f10510675b129ba272ee56693da2249c4a22
cloud-cb <646932161@qq.com>
2017-05-02 15:38:52 +0800

    完成注册功能

--- src/pages/formPages/registerPage/registerPage.vue
+++ src/pages/formPages/registerPage/registerPage.vue
@@ -29,53 +29,59 @@
         <div class="tel list">
           <span class="left">手机号：</span>
           <div class="right">
-            <input class="input" type="tel" maxlength="11" placeholder="请输入注册手机号">
+            <div class="tip" v-if="tip.userMobileTip">{{tip.userMobileTip}}</div>
+            <input @blur="blurUserMobile" v-model="registerData.userMobile" :class="{warn: tip.userMobileTip}" class="input" type="tel" maxlength="11" placeholder="请输入注册手机号">
           </div>
         </div>
 
         <div class="tel list">
           <span class="left">密码：</span>
           <div class="right">
-            <input class="input" type="password" maxlength="11" placeholder="请输入密码">
+            <div class="tip" v-if="tip.userPWDTip">{{tip.userPWDTip}}</div>
+            <input @blur="blurUserPWD" v-model="registerData.userPWD" :class="{warn: tip.userPWDTip}" class="input" type="password" maxlength="16" placeholder="请输入密码">
           </div>
         </div>
 
         <div class="tel list">
           <span class="left">确认密码：</span>
           <div class="right">
-            <input class="input" type="password" maxlength="11" placeholder="请再次输入密码">
+            <div class="tip" v-if="tip.confirmPWDTip">{{tip.confirmPWDTip}}</div>
+            <input @blur="blurConfirmPWD" v-model="confirmPWD" :class="{warn: tip.confirmPWDTip}" class="input" type="password" maxlength="16" placeholder="请再次输入密码">
           </div>
         </div>
 
         <div class="tel list">
           <span class="left">姓名：</span>
           <div class="right">
-            <input class="input" type="text" maxlength="11" placeholder="请输入你的名字">
+            <div class="tip" v-if="tip.userNameTip">{{tip.userNameTip}}</div>
+            <input @blur="blurUserName" v-model="registerData.userName" :class="{warn: tip.userNameTip}" class="input" type="text" maxlength="8" placeholder="请输入你的名字">
           </div>
         </div>
 
         <div class="tel list">
           <span class="left">公司：</span>
           <div class="right">
-            <input class="input" type="text" maxlength="11" placeholder="情输入你的公司名">
+            <div class="tip" v-if="tip.companyNameTip">{{tip.companyNameTip}}</div>
+            <input @blur="blurCompanyName" v-model="registerData.companyName" :class="{warn: tip.companyNameTip}" class="input" type="text"  placeholder="情输入你的公司名">
           </div>
         </div>
 
         <div class="code list">
           <span class="left">验证码：</span>
           <div class="right">
-            <input class="input" type="tel" maxlength="11" placeholder="请输入短信验证码">
-            <div class="SMSCode">获取验证码</div>
+            <div class="tip" v-if="tip.smsCodeTip">{{tip.smsCodeTip}}</div>
+            <input @blur="blurSmsCode" v-model="registerData.smsCode" :class="{warn: tip.smsCodeTip}" class="input" type="tel" maxlength="4" placeholder="请输入短信验证码">
+            <button @click="getSMSCode" ref="getCodeBtn" :disabled="getSMSCodeParams.disabled" class="SMSCode button button-blue" title="点击获取短信验证码">{{codeBtnText}}</button>
           </div>
         </div>
 
         <div class="btn-wrapper">
-          <button class="button button-block button-blue">注册</button>
+          <button class="button button-block button-blue" @click="register">注册</button>
         </div>
 
         <div class="agreenment-wrapper">
           <i class="iconfont">字</i>
-          <span class="text">用户勾选即代表同意</span>
+          <span class="text">点击注册即代表同意</span>
           <span class="agreenment">《坐视布管用户协议》</span>
         </div>
 
@@ -86,13 +92,15 @@
 
 <script>
 import {header, nav} from '../../../components';
-// import {
-//   testMobile,
-//   testPWD,
-//   testName,
-//   testCompanyName,
-//   testSMSCode
-// } from '../../../common/js/regExp';
+import {
+  testMobile,
+  testPWD,
+  testName,
+  testCompanyName,
+  testSMSCode
+} from '../../../common/js/regExp';
+// import axios from 'axios';
+import {getRegSMSCode, reg, checkPhone} from '../../../common/api/api';
 export default {
   data() {
     return {
@@ -103,12 +111,38 @@ export default {
         userName: '',
         companyName: '',
         smsCode: ''
+      },
+      confirmPWD: '',
+      isRegisted: true,
+      samePWD: true,
+      tip: {
+        userMobileTip: '',
+        userPWDTip: '',
+        confirmPWDTip: '',
+        userNameTip: '',
+        companyNameTip: '',
+        smsCodeTip: ''
+      },
+      getSMSCodeParams: {
+        getCode: '获取验证码',
+        seconds: 60,
+        wait: '秒后获取',
+        disabled: false
       }
     };
   },
   components: {
     'vHeader': header,
     'vNav': nav
+  },
+  computed: {
+    codeBtnText() {
+      if (this.getSMSCodeParams.disabled === false) {
+        return this.getSMSCodeParams.getCode;
+      } else {
+        return this.getSMSCodeParams.seconds + this.getSMSCodeParams.wait;
+      }
+    }
   },
   methods: {
     chooseFactory() {
@@ -120,6 +154,157 @@ export default {
       this.$refs.factory.className = 'type-item';
       this.$refs.stalls.className += ' active';
       this.registerData.userType = 2;
+    },
+    blurUserMobile() {
+      if (!testMobile(this.registerData.userMobile)) {
+        this.tip.userMobileTip = '请输入正确的手机号码';
+        return;
+      } else {
+        this.tip.userMobileTip = '';
+      }
+      console.log(this.registerData.userMobile);
+      checkPhone({
+        mobile: this.registerData.userMobile
+      }).then(res => {
+        if (res.data.code !== 0) {
+          console.log('检查手机号码失败', res);
+        } else {
+          if (res.data.data === true) {
+            this.tip.userMobileTip = '此手机号码已被注册';
+          } else {
+            this.isRegisted = false;
+          }
+        }
+      }).catch(res => {
+        console.log('检查手机号码请求失败', res);
+      });
+    },
+    blurUserPWD() {
+      if (!testPWD(this.registerData.userPWD)) {
+        this.tip.userPWDTip = '密码长度为6-16位';
+      } else {
+        this.tip.userPWDTip = '';
+      }
+    },
+    blurConfirmPWD() {
+      if (!testPWD(this.confirmPWD)) {
+        this.tip.confirmPWDTip = '两次密码输入不一致';
+      }
+      if (this.confirmPWD !== this.registerData.userPWD) {
+        this.tip.confirmPWDTip = '两次密码输入不一致';
+      } else {
+        this.tip.confirmPWDTip = '';
+        this.samePWD = false;
+      }
+    },
+    blurUserName() {
+      if (!testName(this.registerData.userName)) {
+        this.tip.userNameTip = '姓名长度为2-8位汉字';
+      } else {
+        this.tip.userNameTip = '';
+      }
+    },
+    blurCompanyName() {
+      if (!testCompanyName(this.registerData.companyName)) {
+        this.tip.companyNameTip = '公司名称长度为4-16位';
+      } else {
+        this.tip.companyNameTip = '';
+      }
+    },
+    blurSmsCode() {
+      if (!testSMSCode(this.registerData.smsCode)) {
+        this.tip.smsCodeTip = '请输入4位短信验证码';
+      } else {
+        this.tip.smsCodeTip = '';
+      }
+    },
+    getSMSCode() {
+      if (!testMobile(this.registerData.userMobile)) {
+        this.tip.userMobileTip = '请输入正确的手机号码';
+        return;
+      } else {
+        this.tip.userMobileTip = '';
+      }
+      let data = {
+        mobile: this.registerData.userMobile
+      };
+      getRegSMSCode(data)
+        .then(res => {
+          if (res.data.code !== 0) {
+            // 手机号码已注册的情况
+            if (res.data.code === 1004019) {
+              this.tip.userMobileTip = res.data.message;
+            }
+          } else {
+            console.log('短信获取成功', res);
+            this.getSMSCodeParams.disabled = true;
+            let timer = setInterval(() => {
+              if (this.getSMSCodeParams.seconds < 1) {
+                this.getSMSCodeParams.seconds = 60;
+                this.getSMSCodeParams.disabled = false;
+                clearInterval(timer);
+              }
+              this.getSMSCodeParams.seconds -= 1;
+            }, 1000);
+          }
+        })
+        .catch(res => {
+          console.error('获取短信请求错误', res);
+        });
+    },
+    register() {
+      let _this = this;
+      let data = this.registerData;
+      for (let key in data) {
+        if (!data.hasOwnProperty(key)) {
+          continue;
+        }
+        // 点击注册时表单漏填的情况
+        if (!data[key]) {
+          console.log(data[key]);
+          console.log(key + 'Tip');
+          switch (key) {
+            case 'userType':
+              alert('请至少选择一个注册身份');
+              return;
+            case 'userPWD':
+              alert('请输入正确的密码');
+              return;
+            case 'userName':
+              alert('请输入您的姓名');
+              return;
+            case 'companyName':
+              alert('请输入公司名称');
+              return;
+            case 'smsCode':
+              alert('请输入短信验证码');
+              return;
+            default:
+              alert('请填写正确的注册信息');
+              return;
+          }
+        }
+      }
+      let _data = this.registerData;
+      let checkPassword = _data.userPWD === this.confirmPWD;
+      if (!(testMobile(_data.userMobile) && testPWD(_data.userPWD) && testName(_data.userName) && testCompanyName(_data.companyName) && testSMSCode(_data.smsCode) && checkPassword && !this.isRegisted && !this.samePWD)) {
+        alert('请填写正确的注册信息');
+        return;
+      }
+      reg(this.registerData)
+        .then(res => {
+          if (res.data.code !== 0) {
+            alert(res.data.message);
+          } else {
+            alert('注册成功！');
+            _this.$router.push({
+              path: '/loginPage'
+            });
+          }
+        })
+        .catch(res => {
+          console.error('注册失败', res);
+        });
     }
   }
 };
@@ -128,6 +313,7 @@ export default {
 <style lang="stylus" scoped>
   .input
     display inline-block
+    position relative
     height 40px
     line-height 40px
     padding-left 16px
@@ -137,6 +323,9 @@ export default {
     border 1px solid rgba(7, 17, 27, 0.2)
     &::-webkit-input-placeholder
       font-size 16px
+    &.warn
+      border 1px solid rgba(245, 53, 3, 0.6)
+      box-shadow 0 0 5px rgba(245, 53, 3, 0.3)
   .register-page
     .register-box
       width 100%
@@ -169,7 +358,7 @@ export default {
             .link
               color #4c93fd
         .list
-          margin 16px 0
+          margin 22px 0
           .left
             display inline-block
             vertical-align top
@@ -181,7 +370,14 @@ export default {
             text-align center
           .right
             display inline-block
+            position relative
             vertical-align top
+            .tip
+              position absolute
+              z-index 1
+              top -16px
+              font-size 14px
+              color #f53535
           &.choose-type
             margin 30px 0
             .left
@@ -228,7 +424,6 @@ export default {
               text-align center
               cursor pointer
               color #fff
-              background #4c93fd
         .btn-wrapper
           margin-top 32px
           height 48px
