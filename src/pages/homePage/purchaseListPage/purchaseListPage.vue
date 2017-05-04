5ad30e18d5e138a087f43d141b4d8aca20b84379
cloud-cb <646932161@qq.com>
2017-05-04 19:02:01 +0800

    新增求购列表页 和 供应列表页，待完成

---
+++ src/pages/homePage/purchaseListPage/purchaseListPage.vue
@@ -0,0 +1,35 @@
+
+<template>
+  <div class="purchase-list-page">
+    <v-header></v-header>
+    <v-nav></v-nav>
+    <div class="purchase-list-page-box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  header,
+  nav
+} from '../../components';
+
+export default {
+  components: {
+    'vHeader': header,
+    'vNav': nav
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.purchase-list-page
+  background #f2f2f2
+  .purchase-list-page-box
+    width 1200px
+    margin 0 auto
+    margin-top 300px
+    padding 16px 0
+    background #f2f2f2
+</style>
---
+++ src/pages/homePage/supplyListPage/supplyListPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="supply-list-page">
+    <v-header></v-header>
+    <v-nav></v-nav>
+    <div class="supply-list-page-box">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  header,
+  nav
+} from '../../components';
+
+export default {
+  components: {
+    'vHeader': header,
+    'vNav': nav
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+  .supply-list-page
+    background #f2f2f2
+    .supply-list-page-box
+      width 1200px
+      margin 0 auto
+      margin-top 300px
+      padding 16px 0
+      background #f2f2f2
+</style>
