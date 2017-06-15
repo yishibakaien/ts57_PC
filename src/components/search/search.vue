e323e305990c11bf124f1fdf990362245b1c2bb5
hopkinson <1651690426@qq.com>
2017-06-15 10:29:09 +0800

    11

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -12,7 +12,9 @@
     <input type="text" class="search-input" :placeholder="inputPlaceHolder" v-model="Search.val" @keyup.enter.native="handleSearch" @focus="handleDelPic" :disabled="search.handleStatus">
     <i class="icon-xiangji add-upload-button" v-show="!Pic.isUploaded" @click="Pic.isUploaded=true,Search.val=''" v-if="!search.handleStatus"></i>
     <span class="search-close" v-show="Pic.isUploaded" @click="Pic.isUploaded=false">&times;</span>
-    <ts-button type="primary" class="search-button" @click="handleSearch" v-show="!Pic.isUploaded" :disabled="search.handleStatus">搜 索</ts-button>
+    <ts-button type="primary" class="search-button" @click="handleSearch" v-show="!Pic.isUploaded" :disabled="search.handleStatus">
+      <i class="icon-xiangji"></i> 拍照搜花
+    </ts-button>
   </div>
   <!-- 上传花型  -->
   <div class="search-editPic onepx" v-show="Pic.isUploaded">
